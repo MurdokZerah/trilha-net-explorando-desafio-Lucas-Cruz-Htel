9b72c1aabd495e9003ce42fc3ee984b48916c97b
Lucas Cruz <138832464+MurdokZerah@users.noreply.github.com>
2025-08-24 22:04:19 -0300

    Update Suite.cs

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -2,10 +2,15 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Suite
     {
-        public Suite() { }
-
         public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
         {
+            if (string.IsNullOrWhiteSpace(tipoSuite))
+                throw new ArgumentException("Tipo da suíte deve ser informado.", nameof(tipoSuite));
+            if (capacidade <= 0)
+                throw new ArgumentException("Capacidade deve ser maior que zero.", nameof(capacidade));
+            if (valorDiaria < 0)
+                throw new ArgumentException("Valor da diária não pode ser negativo.", nameof(valorDiaria));
+
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
